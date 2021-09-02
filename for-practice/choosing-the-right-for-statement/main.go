44f3829bb3a6e093684ec4dac8746a7961774035
Sho372 <32non23ca@gmail.com>
2021-09-02 15:51:28 +0900

    Chapter 4 Choosing the right for statement

---
+++ for-practice/choosing-the-right-for-statement/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	evenVals := []int{2, 4, 6, 8, 10, 12}
+	for i, v := range evenVals {
+		if i == 0 || i == len(evenVals)-1 {
+			continue
+		}
+		fmt.Println(i, v)
+	}
+
+	fmt.Println()
+
+	for i := 1; i < len(evenVals) - 1; i++ {
+		fmt.Println(i, evenVals[i])
+	}
+}
