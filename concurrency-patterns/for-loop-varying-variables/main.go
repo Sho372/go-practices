f24de397a40db5b10bcc41ec37c2c133c5e9607b
Sho372 <32non23ca@gmail.com>
2023-08-20 09:49:04 +0900

    Learning Go: P.213 Goroutines, for Loops, and Varying Variables

--- concurrency-patterns/for-loop-varying-variables/main.go
+++ concurrency-patterns/for-loop-varying-variables/main.go
@@ -8,9 +8,11 @@ func main() {
 	a := []int{2, 4, 6, 8, 10}
 	ch := make(chan int, len(a))
 	for _, v := range a {
-		go func() {
-			ch <- v * 2
-		}()
+		//v := v //shadow
+		go func(val int) {
+			//ch <- v * 2
+			ch <- val * 2
+		}(v)
 		//time.Sleep(time.Second) //vの更新を遅らせれれば期待した動きをする
 	}
 	for i := 0; i < len(a); i++ {
