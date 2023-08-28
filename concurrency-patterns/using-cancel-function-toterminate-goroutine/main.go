a8c215c02a07103b4ff6ee867a5ceee13ff39f27
Sho372 <32non23ca@gmail.com>
2023-08-28 13:21:29 +0900

    Learning Go: P.216 Using a Cancel Function to Terminate a Goroutine

---
+++ concurrency-patterns/using-cancel-function-toterminate-goroutine/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func countTo(max int) (<-chan int, func()) {
+	ch := make(chan int)        // for returns data
+	done := make(chan struct{}) // for signaling done
+	cancel := func() {          // closure includes close func
+		fmt.Println("in cancel: execute close")
+		close(done)
+	}
+	go func() {
+		for i := 0; i < max; i++ {
+			select {
+			case <-done:
+				return
+			default:
+				ch <- i
+			}
+		}
+		close(ch)
+	}()
+	return ch, cancel
+}
+
+func main() {
+
+	ch, cancel := countTo(10)
+	for i := range ch {
+		if i > 5 {
+			break
+		}
+		fmt.Println(i)
+	}
+	cancel()
+
+}
